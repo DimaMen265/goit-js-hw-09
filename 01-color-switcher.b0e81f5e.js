c3d2768b5dd02bf6408e5ddcf2a44e0387033b6e
DimaMen265 <134741876+DimaMen265@users.noreply.github.com>
2023-10-09 13:23:23 +0000

    Deploying to gh-pages from @ DimaMen265/goit-js-hw-09@14fdfe3d55d987a0e2c22aed2fc6dd03f8ade4cf 🚀

---
+++ 01-color-switcher.b0e81f5e.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");e.setAttribute("disabled","");var r=document.querySelector("body"),a=null;t.addEventListener("click",(function(){a=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.setAttribute("disabled",""),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(a),t.removeAttribute("disabled"),e.setAttribute("disabled","")}))}();
+//# sourceMappingURL=01-color-switcher.b0e81f5e.js.map
