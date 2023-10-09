14fdfe3d55d987a0e2c22aed2fc6dd03f8ade4cf
DimaMen265 <menzak87@gmail.com>
2023-10-09 15:22:52 +0200

    The end task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const start = document.querySelector("[data-start]");
+const end = document.querySelector("[data-stop]");
+end.setAttribute("disabled", "");
+const body = document.querySelector("body");
 
+const getRandomHexColor = () => {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+let interval = null;
+
+start.addEventListener("click", () => {
+    interval = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+
+    start.setAttribute("disabled", "");
+    end.removeAttribute("disabled");
+});
+
+end.addEventListener("click", () => {
+    clearInterval(interval);
+
+    start.removeAttribute("disabled");
+    end.setAttribute("disabled", "")
+});
